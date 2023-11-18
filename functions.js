915a003aab7a326c14d55ccc82075cdfeb78a211
ice1ze <nara.wan@dome.tu.ac.th>
2023-11-18 19:27:34 +0700

    update

--- functions.js
+++ functions.js
@@ -11,13 +11,41 @@ function checkScore(score){
     }
   }
 
+function checkCoin(score){
+    const coin = document.getElementById('coin').textContent    
+    const sumcoin = Number(score) + Number(coin) ;   
+    return sumcoin;  
+    }
+
+    function showhighScore() {
+      list().then(response => {
+        const highscoreElement = document.getElementById('highscore');
+        highscoreElement.textContent = response.documents[0].highscore;
+        console.log(response.documents[0].highscore);
+      });
+    }
+    
+    function showCoin(){
+      list().then(response => {
+        const coinElement = document.getElementById('coin');
+        coinElement.textContent = response.documents[0].coin;
+        console.log(response.coin);
+      });       
+    }
+
 
 
 if (typeof global !== 'undefined') {
   global.checkScore = checkScore;
+  global.checkCoin = checkCoin;
+  global.showhighScore = showhighScore;
+  global.showCoin = showCoin;
 }
 
 // แนบ checkScore กับ window object สำหรับบราวเซอร์
 if (typeof window !== 'undefined') {
   window.checkScore = checkScore;
+  window.checkCoin = checkCoin;
+  window.showhighScore = showhighScore;
+  window.showCoin = showCoin;
 }
--- unit-test-jest/test.js
+++ unit-test-jest/test.js
@@ -7,26 +7,31 @@ beforeEach(() => {
 });
 
 describe('checkScore function', () => {
-  test('should return the higher score if the provided score is greater than the current high score', () => {
+  test('TC1: Should display the highest score if the player has existing score data', () => {
     document.body.innerHTML = '<div id="highscore">50</div>';
 
     const result = global.checkScore(70);
     expect(result).toBe(70);
   });
 
-  test('should return the current high score if the provided score is equal to the current high score', () => {
+  test('TC2: Should update the highest score if the player achieves a new high score', () => {
     document.body.innerHTML = '<div id="highscore">80</div>';
 
-    const result = global.checkScore(80);
-    expect(parseInt(result)).toBe(80); // แก้ไขการเปรียบเทียบด้วยการแปลงค่าเป็น number ก่อนเปรียบเทียบ
+    const result = global.checkScore(85);
+    expect(parseInt(result)).toBe(85);
   });
 
-  test('should return the current high score if the provided score is less than the current high score', () => {
+  test('TC3: Should not change the highest score if the player achieves a score equal to the existing high score', () => {
     document.body.innerHTML = '<div id="highscore">90</div>';
 
+    const result = global.checkScore(90);
+    expect(parseInt(result)).toBe(90);
+  });
+
+  test('TC4: Should not change the highest score if the player achieves a score lower than the existing high score', () => {
+    document.body.innerHTML = '<div id="highscore">95</div>';
+
     const result = global.checkScore(85);
-    expect(parseInt(result)).toBe(90); // แก้ไขการเปรียบเทียบด้วยการแปลงค่าเป็น number ก่อนเปรียบเทียบ
+    expect(parseInt(result)).toBe(95);
   });
 });
-
-
